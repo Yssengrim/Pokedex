b35d99b10f024681d96bf475621f565c7b59cd2b
Yssengrim <eisengrin@gmail.com>
2025-05-20 12:34:20 -0500

    Pokedex Commit 1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Yssengrim/Pokedex
+
+go 1.24.1
---
+++ repl_test.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  ",
+			expected: []string{},
+		},
+		{
+			input:    "  hello  ",
+			expected: []string{"hello"},
+		},
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "  HellO  World  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "\thello\tworld\n",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "hello, world!",
+			expected: []string{"hello,", "world!"},
+		},
+		{
+			input:    "hello world! 123",
+			expected: []string{"hello", "world!", "123"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if len(actual) != len(c.expected) {
+			t.Errorf("lengths don't match: '%v' vs '%v'", actual, c.expected)
+			continue
+		}
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf("cleanInput(%v) == %v, expected %v", c.input, actual, c.expected)
+			}
+		}
+	}
+}
